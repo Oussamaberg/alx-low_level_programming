17f8692126311f9d368aa7560829817a1d625860
oussama berghai <ossama.berghai@gmail.com>
2023-03-17 15:15:02 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry piont
+ * generat a random int var n
+ * Return: positive if the n is > 0 or negative if n < 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		else
+			putchar('\n');
+	}
+	putchar('\n');
+
+	return (0);
+}
