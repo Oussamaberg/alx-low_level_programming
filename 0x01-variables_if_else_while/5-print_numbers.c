e078175b99d5039ef9731beff42aff3406a916e0
oussama berghai <ossama.berghai@gmail.com>
2023-03-17 14:33:58 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry piont
+ * generat a random int var n
+ * Return: positive if the n is > 0 or negative if n < 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	putchar('\n');
+
+	return (0);
+}
