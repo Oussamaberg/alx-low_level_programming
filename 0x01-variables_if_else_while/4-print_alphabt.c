946c24bc1b61350698b761bef4b053c9bd8968ac
oussama berghai <ossama.berghai@gmail.com>
2023-03-17 14:26:53 -0700

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry piont
+ * generat a random int var n
+ * Return: positive if the n is > 0 or negative if n < 0
+ */
+int main(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		if (alphabet[i] != 'e' && alphabet[i] != 'q')
+			putchar(alphabet[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
