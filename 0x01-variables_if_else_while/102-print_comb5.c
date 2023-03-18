1abb3867c13e87f9afb3c549efd1f95b337cbd67
oussama berghai <ossama.berghai@gmail.com>
2023-03-17 17:52:20 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry piont
+ * generat a random int var n
+ * Return: positive if the n is > 0 or negative if n < 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int l;
+	int m;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			for (l = 48; l < 58; l++)
+			{
+				for (m = 48; m < 58; m++)
+				{
+					if ((i * 10 + j) < (l * 10 + m))
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(l);
+						putchar(m);
+						if (i == 57 && j == 56)
+							putchar('\n');
+						else
+							putchar(',');
+					}
+				}
+			}
+		}
+	}
+
+	return (0);
+}
