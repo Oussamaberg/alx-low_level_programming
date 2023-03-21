d4a66175f281eff2d3240a577fd1a6c3ecb00519
oussama berghai <ossama.berghai@gmail.com>
2023-03-21 08:53:50 -0700

    Show alphabet with functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+/**
+ * print_alphabet prints the alphabet, in lowercase, followed by a new line.
+ *
+ *
+ *  Return: On success void.
+ **/
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
