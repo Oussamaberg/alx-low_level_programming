e2a0d89ce292177ed8084a5915c471c88d7da117
oussama berghai <ossama.berghai@gmail.com>
2023-03-21 13:17:14 -0700

    is alphabetic

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 /**
- * _islower - checks for lowercase or uppercase character.
+ * _isalpha - checks for lowercase or uppercase character.
  * @c: the character to check.
  * Return: 1 if the character c is lowercase or upper.
  * 0 is returned if c is not an alphabetic character.
  */
-int _islower(int c)
+int _isalpha(int c)
 {
 	if ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A'))
 		return (1);
