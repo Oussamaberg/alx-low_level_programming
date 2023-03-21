be83fb710f317397ae39a2f60e12fbdfb16fb529
oussama berghai <ossama.berghai@gmail.com>
2023-03-21 09:06:28 -0700

    Show alphabet 10 times

--- 0x02-functions_nested_loops/2-prnt_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-prnt_alphabet_x10.c
@@ -2,14 +2,14 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints the alphabet, in lowercase 10 times, followed by a new line.
- *
- *
- *  Return: On success void.
+ * print_alphabet_x10 - prints the alphabet,
+ * in lowercase 10 times,
+ * followed by a new line.
+ * Return: On success void.
  **/
 void print_alphabet_x10(void)
 {
-	int i=1;
+	int i = 1;
 
 	while (i <= 10)
 	{
