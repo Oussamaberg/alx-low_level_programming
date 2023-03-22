0fb778b50b421bd4da3246674aee1a2dd23ec8f3
oussama berghai <ossama.berghai@gmail.com>
2023-03-22 02:44:09 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+/**
+ * _abs - print the absolute value of n.
+ * @n: the number to computes the abs value.
+ * Return: the absolute value of an integer n.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	}
+		return (n);
+	}
+	else
+		return (-n)
+
+}
