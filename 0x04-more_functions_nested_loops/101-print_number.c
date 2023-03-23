b8f90d7abaa5d210fc6f3d22c72da9cbed3bcaa4
oussama berghai <ossama.berghai@gmail.com>
2023-03-23 13:53:21 -0700

    Numbers have life

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,16 +7,18 @@
  */
 void print_number(int n)
 {
+	int MAX = 1000000000;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	if (n >= 1000)
-		_putchar((n / 1000) + '0');
-	if (n >= 100)
-		_putchar((n / 100) % 10 + '0');
-	if (n >= 10)
-		_putchar((n / 10) % 10 + '0');
-	_putchar(n % 10 + '0');
+	while (MAX > 1)
+	{
+		if ((n / MAX) >= 1)
+			_putchar((n / MAX) % 10 + '0');
+		MAX = MAX / 10;
+	}
+	_putchar((n % 10) + '0');
 }
