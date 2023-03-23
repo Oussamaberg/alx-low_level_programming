4f8ecc791c8bde40dc87efda5e0ec0012c258cb0
oussama berghai <ossama.berghai@gmail.com>
2023-03-22 19:03:23 -0700

    n times table

--- 0x02-functions_nested_loops/100-times_table_old.c
+++ 0x02-functions_nested_loops/100-times_table_old.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * print_times_table - prints the n times table, starting with 0.
  * @n: the number to calculate times table causes an infinite loop
@@ -18,24 +18,37 @@ void print_times_table(int n)
 			break;
 		i = 0;
 		num = 0;
-		printf("0");
-		if (n == 0)
-			printf("\n");
+		_putchar('0');
+		if(n == 0)
+			_putchar('\n');
 		else
-			printf(",");
+			_putchar(',');
 		while (i < n)
 		{
 			num = num + times;
 			if (num <= 9)
-				printf("   %d", num);
-			else if (num < 100)
-				printf("  %d", num);
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(num + '0');
+			}
 			else
-				printf(" %d", num);
+			{
+				_putchar(' ');
+				if (num >= 100)
+					_putchar((num / 100) % 10 + '0');
+				else
+					_putchar(' ');
+				_putchar((num / 10) % 10 + '0');
+				_putchar(num % 10 + '0');
+			}
 			if (i == n - 1)
-				printf("\n");
+				_putchar('\n');
 			else
-				printf(",");
+			{
+				_putchar(',');
+			}
 			i++;
 		}
 		times++;
