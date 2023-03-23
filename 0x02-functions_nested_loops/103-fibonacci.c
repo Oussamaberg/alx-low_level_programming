d29f765564174f789b996762c046f806dce7a4df
oussama berghai <ossama.berghai@gmail.com>
2023-03-22 19:57:43 -0700

    super long fibonacci even sum

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	long a = 1;
 	long b = 2;
-	long c;
+	long c = 0;
 	long sum = 2;
 	int max_val = 4000000;
 
@@ -18,7 +18,6 @@ int main(void)
 		c = a + b;
 		if (c % 2 == 0)
 			sum = sum + c;
-		printf("%ld",sum);
 		a = b;
 		b = c;
 	}
