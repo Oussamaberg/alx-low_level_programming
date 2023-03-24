8cbc6621469245957b3c7034a0a6e0b7a9c055a7
oussama berghai <ossama.berghai@gmail.com>
2023-03-23 18:11:19 -0700

    I am a diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draw a diagonal line.
+ * @n: is the number of times the character \ should be printed
+ * Return: Void.
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n ; i++)
+		{
+			for (j = 0; j <= i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
