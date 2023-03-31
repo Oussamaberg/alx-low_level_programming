8cbcaf581f80ab3676f2a8a33b1705a01d6c8a71
oussama berghai <ossama.berghai@gmail.com>
2023-03-30 20:17:09 -0700

    Rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,20 +2,32 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * leet - encodes a string into rot13.
+ * rot13 - encodes a string into rot13.
  * @s: string.
  * Return: s.
  */
 char *rot13(char *s)
 {
 	int i = 0;
+	int j;
+	char arr1[52] = "aAbBcCdDeEfFgGhHiIjJkKlLmM";
+	char arr2[52] = "nNoOpPqQrRsStTuUvVwWxXyYzZ";
+	char arr[26];
 
+	strcpy(arr, arr1);
+	strcat(arr1, arr2);
+	strcat(arr2, arr);
 	while (s[i] != '\0')
 	{
-		if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
-			s[i] = s[i] + 13;
-		else if ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
-			s[i] = s[i] - 13;
+		j = 0;
+		for (; j < 52; j++)
+		{
+			if (s[i] == arr1[j])
+			{
+				s[i] = arr2[j];
+				break;
+			}
+		}
 		i++;
 	}
 	return (s);
