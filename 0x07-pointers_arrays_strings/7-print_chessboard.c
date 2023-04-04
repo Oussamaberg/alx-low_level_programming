05547d4ed8429334f7869bfefa6007acce656629
oussama berghai <ossama.berghai@gmail.com>
2023-04-03 18:19:07 -0700

    chess Board

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -13,8 +13,8 @@ void print_chessboard(char (*a)[8])
 
 	for (i = 0; i < 8; i++)
 	{
-		for(j = 0; j < 8; j++)
-			printf("%c",a[i][j]);
+		for (j = 0; j < 8; j++)
+			printf("%c", a[i][j]);
 		printf("\n");
 	}
 
