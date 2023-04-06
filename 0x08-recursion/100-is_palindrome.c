6e8b44990550390582e4279baccd14a954af6f45
oussama berghai <ossama.berghai@gmail.com>
2023-04-05 18:56:53 -0700

    proud of my palindrome algo

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<string.h>
+/**
+ * palindrome - returns 1 if a string is a palindrome and 0 if not.
+ * @s: pointer to the beguinin of the string.
+ * @s_end: point to the end
+ *
+ * Return: 1 if palindrome.
+ * 0 if it's not.
+ */
+int palindrome(char *s, char *s_end)
+{
+	if (s_end > s)
+	{
+		if (*s == *(s_end))
+		{
+			palindrome(s + 1, s_end - 1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	else
+		return (1);
+}
+/**
+ *  is_palindrome - returns 1 if a string is a palindrome and 0 if not.
+ * @s: a string.
+ *
+ * Return: 1 if palindrome 0 if it's not.
+ */
+int is_palindrome(char *s)
+{
+	char *s_end;
+
+	s_end = s + strlen(s) - 1;
+		return (palindrome(s, s_end));
+}
