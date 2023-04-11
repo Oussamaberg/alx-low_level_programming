e1090dab49ce3c5a53b8f540a33112d58e04c955
oussama berghai <ossama.berghai@gmail.com>
2023-04-11 14:21:59 -0700

    say my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,9 @@
  * @argv: arguments vector.
  * Return: always 0.
  */
-int main(int argc, char *argv[])
+int main(int argc,char *argv[])
 {
-	printf("%s\n", argv[0]);
+	if (argc >= 1)
+		printf("%s\n", argv[0]);
 	return (0);
 }
