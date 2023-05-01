72c623b5cfbda5bf0edfd0bff1a085da8414c905
oussama berghai <ossama.berghai@gmail.com>
2023-05-01 10:33:06 -0700

    Insert by index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,6 +21,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 	while (element)
 	{
 		if (index == (idx - 1))
