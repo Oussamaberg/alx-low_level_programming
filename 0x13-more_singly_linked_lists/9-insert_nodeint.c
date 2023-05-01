9976b82585b445e526d4ef1c5dc4d3f556fc748e
oussama berghai <ossama.berghai@gmail.com>
2023-05-01 10:27:01 -0700

    Insert by index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	element = *head;
 	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	if (!new || !head || !*head )
 	{
 		return (NULL);
 	}
