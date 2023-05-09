b36dd5adf3bf3c72db7f7ccdb693ba5f89aae908
oussama berghai <ossama.berghai@gmail.com>
2023-05-09 09:23:41 -0700

    File IO

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,12 +17,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (l = 0; text_content[len];)
+		for (l = 0; text_content[l];)
 			l++;
 	}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
+	w = write(o, text_content, l);
 
 	if (o == -1 || w == -1)
 		return (-1);
